ac9d625549f0fc2d70d46eb1960128cb673924b7
Brendan Tang <pikapwn516@nat-wireless-guest-reg-153-50.bu.edu>
2022-03-30 13:10:28 -0400

    Created StatusMessage model, show status messages on profile page

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -11,6 +11,22 @@ class Profile(models.Model):
 
     def __str__(self):  
         return f"{self.first_name}-{self.last_name}-{self.email}-{self.image}"
+    
+    def get_status_messages(self):
+        '''Return all quotes for this Person.'''
+
+        # use the object manager to filter Quotes by this person's pk:
+        return StatusMessage.objects.filter(profile=self)
+
+class StatusMessage(models.Model):
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(blank=True)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    def __str__(self):
+        '''Return a string representation of the status message'''
+
+        return str(self.time_stamp) + self.message 
+
 
 
           
